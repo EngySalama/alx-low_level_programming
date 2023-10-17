a648f29883d4a2daeeab6646837e3a9621c2a8fa
EngySalama <engysalama1992@gmail.com>
2023-10-17 06:08:34 -0700

    a

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,5 @@
 i#include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 int append_text_to_file(const char *filename, char *text_content) {
     if (filename == NULL || text_content == NULL) {
